035b2594730c9816befb8481e1653184a088fd66
18738529936 <31152365+18738529936@users.noreply.github.com>
2017-08-19 14:31:25 +0800

    Add files via upload

---
+++ spring-cloud/feign/src/main/java/com/duanrong/feign/service/SchedualServiceHiHystrix.java
@@ -0,0 +1,11 @@
+package com.duanrong.feign.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystrix implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry, " + name;
+    }
+}
